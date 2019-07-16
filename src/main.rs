87031a3ee067d3ed1b5a90e6bce4425322bbeb2f
kmkzt <info.pscreator@gmail.com>
2019-07-16 23:05:25 +0900

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate image;
+
+use image::GenericImageView;
+
+fn main() {
+    // Use the open function to load an image from a Path.
+    // ```open``` returns a `DynamicImage` on success.
+    let img = image::open("src/tests/images/jpg/cat.jpg").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("test.png").unwrap();
+}
