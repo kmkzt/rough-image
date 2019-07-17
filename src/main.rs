6be1cd33bd120912e40c0944638c10115fa42527
kmkzt <info.pscreator@gmail.com>
2019-07-17 22:28:17 +0900

    feat: parse argv image path

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 extern crate image;
 
 use image::GenericImageView;
+use std::io::Write;
+use std::env::args;
 
 fn main() {
     // Use the open function to load an image from a Path.
     // ```open``` returns a `DynamicImage` on success.
-    let img = image::open("src/tests/images/jpg/cat.jpg").unwrap();
+    let mut image_path: Vec<String> = Vec::new();
 
-    // The dimensions method returns the images width and height.
-    println!("dimensions {:?}", img.dimensions());
+    for arg in args().skip(1) {
+        image_path.push(arg);
+    }
 
-    // The color method returns the image's `ColorType`.
-    println!("{:?}", img.color());
+    if image_path.len() == 0 {
+        writeln!(std::io::stderr(), "Usage rough-image image/path.jpg ...").unwrap();
+        std::process::exit(1);
+    }
 
-    // Write the contents of this image to the Writer in PNG format.
-    img.save("test.png").unwrap();
+    for path in image_path {
+        let img = image::open(path).unwrap();
+        // The dimensions method returns the images width and height.
+        println!("dimensions {:?}", img.dimensions());
+
+        // The color method returns the image's `ColorType`.
+        println!("{:?}", img.color());
+
+        // Write the contents of this image to the Writer in PNG format.
+        img.save("test.png").unwrap();
+    }
 }
